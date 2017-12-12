d753bf13334307e1e3c010a94e8c5286d1b6b46b
tkingless <t.kingless@yahoo.com>
2017-12-12 12:06:13 +0800

    update doc

--- SerialUART/RPi3/RW_RPi3.py
+++ SerialUART/RPi3/RW_RPi3.py
@@ -4,6 +4,7 @@ import time
 wp.wiringPiSetup()
 #RPi3 has changed things around a bit: ttyAMA0 now refers to the serial
 #port that is connected to the bluetooth. The old serial port is now called ttyS0
+#Documentation: wiringpi.com/reference/serial-library/
 serial = wp.serialOpen('/dev/serial0',9600)
 wp.serialPuts(serial,'h')
 
