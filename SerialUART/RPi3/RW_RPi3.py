355e571c92e8abe5cd7f1274a7535e7216e896dd
tkingless <t.kingless@yahoo.com>
2017-12-12 12:05:02 +0800

    RPi3: UART vs arduino

--- SerialUART/RPi3/RW_RPi3.py
+++ SerialUART/RPi3/RW_RPi3.py
@@ -1,6 +1,15 @@
+#!/usr/bin/python
 import wiringpi as wp
+import time
 wp.wiringPiSetup()
 #RPi3 has changed things around a bit: ttyAMA0 now refers to the serial
 #port that is connected to the bluetooth. The old serial port is now called ttyS0
 serial = wp.serialOpen('/dev/serial0',9600)
 wp.serialPuts(serial,'h')
+
+time.sleep(4)
+
+if (wp.serialDataAvail(serial) > 0):
+    print( chr(wp.serialGetchar(serial)) )
+
+wp.serialClose(serial)
