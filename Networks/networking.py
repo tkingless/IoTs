f4db2d5925ee40226b6c1f9b9abebc6ad5fa1cef
tkingless <t.kingless@yahoo.com>
2017-12-14 18:09:30 +0800

    UART: readBytes()

--- Networks/networking.py
+++ Networks/networking.py
@@ -1,36 +1,54 @@
 #Define communication objs
 import pigpio as pigpio
+import time
 
 baudRate = 9600
 bits = 8
+ten_char_time = 100.0/float(baudRate)
 PIobj = pigpio.pi()
 
 class UARTobj(object):
 
 	def __init__(self,TX,RX):
 
-		self.TX = TX
-		self.RX = RX
-		self.wid = None
+	    self.TX = TX
+	    self.RX = RX
+	    self.wid = None
 
-		PIobj.set_mode(TX,pigpio.OUTPUT)
-		PIobj.set_mode(RX,pigpio.INPUT)
+	    PIobj.set_mode(TX,pigpio.OUTPUT)
+	    PIobj.set_mode(RX,pigpio.INPUT)
+	    #PIobj.exceptions = False
+	    #PIobj.bb_serial_read_close(self.RX)
+	    #PIobj.exceptions = True
+	    PIobj.bb_serial_read_open(self.RX,baudRate,bits)
 
         def WriteBytes(self,data):
             self.configureWave(data)
             #wid is created
             PIobj.wave_send_once(self.wid)
 
+        def ReadBytes(self):
+            #count = 1
+            #bytess = bytearray()
+            #while count:
+                #(count,data) = PIobj.bb_serial_read(self.RX)  
+                #if count:
+                    #bytess.append(data)
+                #time.sleep(ten_char_time)
+
+            (count,data) = PIobj.bb_serial_read(self.RX)
+            print "count: {0} data: {1}".format(count,data)
+                
+            return data
+            
         def configureWave(self,data):
             PIobj.wave_clear()
             PIobj.wave_add_serial(self.TX,baudRate,data)
+            while PIobj.wave_tx_busy():
+                pass
             self.wid = PIobj.wave_create()
 
         def Delete(self):
-            PIobj.wave_delete(self.wid)
+            if self.wid is not None:
+                PIobj.wave_delete(self.wid)
             PIobj.bb_serial_read_close(self.RX)
-	#def run(self):
-		#needed to be in main loop for serial reading
-	#def WriteAbyte(self, val):
-	#def ReadAbyte(self, aInput):
-		#return None
