be8175761e874b28f95f6c5a7ef77f33f0ba61d4
tkingless <t.kingless@yahoo.com>
2017-12-15 17:31:35 +0800

    update

--- worker.py
+++ worker.py
@@ -4,10 +4,11 @@ import time
 
 class UARTadapter(threading.Thread):
 
-    def __init__(self):
+    def __init__(self,TX,RX):
         threading.Thread.__init__(self)
         self.HaveReceivedValue = False
-        self.uart = networking.UARTobj(23,24)
+        self.uart = networking.UARTobj(TX,RX)
+        self.callback = None
         return
 
     def run(self):
@@ -15,15 +16,32 @@ class UARTadapter(threading.Thread):
             (cnt,readBytes) = self.uart.ReadBytes()
             time.sleep(float(0.02))
             if cnt > 0:
-                self.HaveReceivedValue = True
+                #self.HaveReceivedValue = True
+                WhenBytesRead(readBytes)
 
-        self.uart.Delete()
+        #self.uart.Delete()
         return
-    
+
+    def WriteBytes(self,data):
+        self.uart.WriteBytes(data)
+        return
+
+    def RegReadCB(self,cb):
+        self.callback = cb
+        return
+
+    def WhenBytesRead(self,data):
+        self.callback(data)
+        return
+
+    def Terminate(self):
+        self.uart.Delete()
+        self.HaveReceivedValue = True
+"""    
 def worker():
     print "Worker init"
 
-    UARTadpt = UARTadapter()
+    UARTadpt = UARTadapter(23,24)
     UARTadpt.start()
 
     while UARTadpt.isAlive():
@@ -35,8 +53,31 @@ t = threading.Thread(target=worker)
 t.start()
 t.join()
 
-print 'Main() finished'
 
+print 'Main() finished'
+"""
+
+"""
+Event Callback pattern test
+"""
+
+class Pub(object):
+
+    def __init__(self,cb):
+        self.callback = cb
+        return
+
+    def Raise(self,arg):
+        self.callback(arg)
+        return
+
+class Sub(object):
+
+    def __init__(self):
+        return
+
+    def CB (self,arg):
+        print ("Hello {}".format(arg))
 
             
 
