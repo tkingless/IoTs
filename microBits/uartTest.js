935df2b7c7263cfb3b0d2ed07cbac6d229aff0d2
FaatJai <chengkafaat@hotmail.com>
2017-12-28 15:42:44 +0800

    Blink eyes in mircobit

--- microBits/uartTest.js
+++ microBits/uartTest.js
@@ -3,13 +3,36 @@ input.onButtonPressed(Button.A, () => {
     serial.writeString("W")
 })
 serial.redirect(
-    SerialPin.P0,
-    SerialPin.P1,
-    BaudRate.BaudRate115200
+SerialPin.P0,
+SerialPin.P1,
+BaudRate.BaudRate115200
 )
 basic.forever(() => {
     readData = serial.readString()
     if (!(readData == "")) {
-        basic.showString(readData)
+        if (readData == "N") {
+            basic.showLeds(`
+                . . # . .
+                # # # # .
+                # . # # .
+                # # # # .
+                . # # # .
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                # # # # .
+                . . . . .
+                . . . . .
+                `)
+            basic.pause(500)
+            basic.showLeds(`
+                . . # . .
+                # # # # .
+                # . # # .
+                # # # # .
+                . # # # .
+                `)
+        }
     }
 })
