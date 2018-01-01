a71e35ebc9e1da6077fac5fdf9326742c726e5b6
tkingless <t.kingless@yahoo.com>
2018-01-01 14:40:46 +0800

    microbit added expression

--- microBits/uartTest.js
+++ microBits/uartTest.js
@@ -1,6 +1,6 @@
 let readData = ""
 input.onButtonPressed(Button.A, () => {
-    serial.writeString("W")
+    
 })
 serial.redirect(
 SerialPin.P0,
@@ -14,36 +14,116 @@ basic.forever(() => {
             basic.showLeds(`
                 . . . . .
                 . . . . .
-                # # # # .
+                . # # # .
                 . . . . .
+                . . . . .
+                `)
+            basic.pause(300)
+            basic.showLeds(`
+                . . . . .
+                . . # . .
+                . # # # .
+                . . # . .
                 . . . . .
                 `)
             basic.pause(500)
             basic.showLeds(`
                 . . # . .
-                # # # # .
-                # . # # .
-                # # # # .
+                . # . # .
+                . # # . .
                 . # # # .
+                . . # . .
                 `)
         }
         if (readData == "A") {
-            basic.showLeds(`
-                # . # . #
-                # # # # #
-                # # # # #
-                # # # # #
-                . # # # .
-                `)
+            for (let i = 0; i < 4; i++) {
+                basic.showLeds(`
+                    . . . # .
+                    . # # . .
+                    # . # . #
+                    # # # # #
+                    . # # # .
+                    `)
+                basic.pause(100)
+                basic.showLeds(`
+                    . . # . .
+                    . # . # .
+                    # # # # #
+                    # . # . #
+                    . # # # .
+                    `)
+                basic.pause(100)
+                basic.showLeds(`
+                    . # . . .
+                    . . # # .
+                    # . # . #
+                    # # # # #
+                    . # # # .
+                    `)
+                basic.pause(100)
+                basic.showLeds(`
+                    . . # . .
+                    . # # # .
+                    # # # # #
+                    # . # . #
+                    . # # # .
+                    `)
+                basic.pause(100)
+            }
         }
         if (readData == "H") {
+            for (let i = 0; i < 4; i++) {
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . # . .
+                    . . . . .
+                    . . . . .
+                    `)
+                basic.pause(200)
+                basic.showLeds(`
+                    . . . . .
+                    . . # . .
+                    . # # # .
+                    . . # . .
+                    . . . . .
+                    `)
+                basic.pause(200)
+                basic.showLeds(`
+                    # . # . #
+                    . # # . .
+                    # # . # #
+                    . # # # .
+                    # . # . #
+                    `)
+                basic.pause(200)
+                basic.showLeds(`
+                    . . . . .
+                    . . # . .
+                    . # # # .
+                    . . # . .
+                    . . . . .
+                    `)
+            }
+        }
+        if (readData == "S") {
+            basic.pause(100)
             basic.showLeds(`
+                . . . . .
                 . . # . .
                 . # # # .
-                # # # # #
-                . # # # .
                 . . # . .
+                . . . . .
                 `)
+            basic.pause(100)
+            basic.showLeds(`
+                . . . # .
+                . . # . .
+                . # # # #
+                . . # . .
+                . . . # .
+                `)
+            basic.pause(500)
         }
     }
 })
